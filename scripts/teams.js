e6e30725f1ce5c497ba3b2198eaaf1680da864ad
BigBadE <bigbade333@gmail.com>
2022-01-26 19:52:19 -0600

    Fixes to team selection

--- scripts/teams.js
+++ scripts/teams.js
@@ -1,40 +1,43 @@
-let teams = []
+let teams = [];
 
 if(document.cookie !== "") {
-    teams = JSON.parse(document.cookie)
+    teams = JSON.parse(document.cookie);
 }
 
-let activeTeam = []
-let activeElement = null
+let activeTeam = [];
+let activeElement = null;
 
 if (teams.length > 0) {
-    activeTeam = teams[0]
+    activeTeam = teams[0];
 }
 
 let teams_element;
 function addTeam() {
-    activeTeam = []
-    teams_element.appendChild(makeTeamElement(activeTeam))
-    teams.add(activeTeam)
+    activeTeam = [];
+    teams_element.prepend(makeTeamElement(activeTeam));
+    teams.push(activeTeam);
 }
 
 function loadTeams() {
     teams_element = document.getElementById("teams");
     for(let i = 0; i < teams.length; i++) {
-        teams.appendChild(makeTeamElement(teams[i]));
+        teams_element.prepend(makeTeamElement(teams[i]));
     }
 }
 
 function selectElement(element) {
-    activeElement.id.substr(activeElement.id.length-9)
-    element.class += " selected"
-    activeElement = element
-    activeTeam = teams[Array.from(element.parentNode.childNodes).indexOf(element)]
+    if(activeElement != null) {
+        activeElement.id = activeElement.id.substr(0, activeElement.id.length - 9);
+    }
+    element.className += " selected";
+    activeElement = element;
+    activeTeam = teams[Array.from(element.parentNode.childNodes).indexOf(element)];
 }
 
 function makeTeamElement(team) {
-    let element = document.getElementById("team-hidden").cloneNode()
-    element.class = element.class.substr(element.class.length-7)
-    element.onclick = () => selectElement(element)
+    let element = document.getElementById("team-hidden").cloneNode();
+    console.log(element + ", " + element.className)
+    element.className = element.className.substr(0, element.className.length-7);
+    element.onclick = () => selectElement(element);
     return element;
 }
